61b9a7f3d2eca8bb866b054741c4bb6b99b8d913
Jaikrishna <47310634+Jaikrishna22@users.noreply.github.com>
2026-09-01 12:44:50 +0530

    Add PWA manifest, service worker, and complete feature summary

---
+++ sw.js
@@ -0,0 +1,31 @@
+﻿const cacheName = 'tnpsc-portal-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/dashboard.html',
+  '/progress-tracker.html',
+  '/leaderboard.html',
+  '/certificates.html',
+  '/current-affairs.html',
+  '/tnpsc-english.html',
+  '/tnpsc-plurals.html',
+  '/tnpsc-quizzes.html',
+  '/question-papers.html',
+  '/answer-keys.html',
+  '/video-solutions.html'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(cacheName)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+      .catch(() => caches.match('/index.html'))
+  );
+});
